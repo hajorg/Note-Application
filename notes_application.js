8be8a005764c2283da68fcccb8969e586356c724
hajorg <jorg2all@yahoo.com>
2016-08-17 15:08:43 +0100

    Notes Application Version 1.1

--- notes_application.js
+++ notes_application.js
@@ -1,16 +1,24 @@
-class NoteApplication {
-	constructor(author) {
+class Note {
+	constructor(author, text) {
 		this.author = author;
+		this.text = text;
+	}
+}
+class NoteApplication {
+	constructor() {
 		this.notes = [];
 	}
 	create(note_content) {
-		this.notes.push(note_content);
+		if (note_content instanceof Note) {
+			this.notes.push(note_content);
+			return this.notes;
+		}
 	}
 	listNotes() {
 		if (this.notes.length > 0) {
 			var contents = "";
 			for (var i = 0; i < this.notes.length; i++){
-				contents += "Note ID: "+ i +"\n"+this.notes[i]+"\nBy Author "+this.author;
+				contents += "Note ID: "+ i +"\n"+this.notes[i]['text']+"\nBy Author "+this.notes[i]['author']+"\n\n";
 			}
 			return contents;
 		}
@@ -19,7 +27,7 @@ class NoteApplication {
 	get(note_id) {
 		if (typeof(note_id) == "number") {
 			if (this.notes.length > note_id) {
-			return this.notes[note_id];
+				return this.notes[note_id]['text'];
 			}else{
 				return "Invalid note id";
 			}
@@ -30,8 +38,8 @@ class NoteApplication {
 	search(search_text) {
 		var found = "";
 		for (var i = 0; i < this.notes.length; i++) {
-			if(this.notes[i].indexOf(search_text) >= 0) {
-				found += "\nNote ID: "+ i +"\n"+this.notes[i]+"\nBy Author "+this.author+"\n";
+			if(this.notes[i].text.indexOf(search_text) > -1) {
+				found += "\nNote ID: "+ i +"\n"+this.notes[i].text+"\nBy Author "+this.notes[i].author+"\n";
 			}
 		}
 		if (found.length > 0) {
